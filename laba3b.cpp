add28c65fe2cfe3a05d9e1b303a53e45e43f4811
ArtemCherepov <130924765+ArtemCherepov@users.noreply.github.com>
2023-05-29 04:30:32 +0300

    Add files via upload

---
+++ laba3b.cpp
@@ -0,0 +1,45 @@
+﻿/*************************************************************************************************************************************************************
+Задать два иерархически связанных полиморфных класса: деталь и сборка (один из них базовый). Все конструкторы находятся в зоне protected (чтобы запретить явное 
+создание). В main() следует создать несколько экземпляров указанных типов, помещая их в хранилище (vector), используя шаблонную дружественную функцию. 
+Дополнить программу всем необходимым для ее правильной работы.
+*************************************************************************************************************************************************************/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Detail {
+protected:
+    Detail() {}
+public:
+    virtual ~Detail() {}
+    template<typename T>
+    friend void add_object(vector<Detail*>& object);
+};
+
+class Build : public Detail {
+protected:
+    Build() = default;
+public:
+    virtual ~Build() {}
+    template<typename T>
+    friend void add_object(vector<Detail*>& object);
+};
+
+template<typename T>
+void add_object(vector<Detail*>& object) {
+    object.push_back(new T());
+}
+
+int main() {
+    vector<Detail*> storage;
+    add_object<Detail>(storage);
+    add_object<Build>(storage);
+    add_object<Detail>(storage);
+    add_object<Build>(storage);
+
+    for (auto i : storage) {
+        delete i;
+    }
+    storage.clear();
+}
